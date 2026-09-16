4c27e7d3f4dcbd2e2f61ce93b4571033472dab38
Baptiste <lemaire.baptiste@gmail.com>
2026-09-16 17:55:03 +0200

    fix port

--- front/js/config.js
+++ front/js/config.js
@@ -3,6 +3,4 @@ const configuredApiUrl = new URLSearchParams(window.location.search).get('api');
 export const API_BASE_URL = configuredApiUrl
     || (window.location.protocol === 'file:'
         ? 'http://localhost:8000'
-        : window.location.port === '8000'
-            ? window.location.origin
-        : `${window.location.protocol}//${window.location.hostname}:8000`);
+        : window.location.origin);
